614f860e1d872a32d616410d1f223acc384cf7de
Tatsuya.Umino <tatsuya.umino@appurunomacbook-puro-2.local>
2018-08-19 01:16:40 +0900

    Initial Commit

---
+++ ARkit_Drawing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARkit_Drawing
+//
+//  Created by Tatsuya.Umino on 2018/08/19.
+//  Copyright © 2018年 Tatsuya.Umino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
